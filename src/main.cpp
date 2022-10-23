7503b1518fee32879209e3ae17ed2b039d290dbf
Marcin <marcin.mrmartian@gmail.com>
2022-10-23 15:33:18 +0100

    Xcode project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Marcin on 23/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
