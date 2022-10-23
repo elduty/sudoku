739a82de1c60b24dadb8e03bc8f4849058c03afd
Marcin <marcin.mrmartian@gmail.com>
2022-10-23 19:10:10 +0100

    BoardTest added

---
+++ include/sudokuTests/BoardTest.h
@@ -0,0 +1,24 @@
+//
+//  BoardTest.h
+//  Sudoku
+//
+//  Created by Marcin on 23/10/2022.
+//
+
+#ifndef BoardTest_h
+#define BoardTest_h
+
+#include "gtest/gtest.h"
+
+namespace sudoku {
+
+class BoardTest : public testing::Test
+{
+protected:
+    void SetUp();
+    void TearDown();
+};
+
+}
+
+#endif /* BoardTest_h */
---
+++ src/sudokuTests/BoardTest.cpp
@@ -0,0 +1,24 @@
+//
+//  BoardTest.cpp
+//  SudokuTests
+//
+//  Created by Marcin on 23/10/2022.
+//
+
+#include <stdio.h>
+#include "BoardTest.h"
+
+using namespace sudoku;
+
+void BoardTest::SetUp()
+{
+}
+
+void BoardTest::TearDown()
+{
+}
+
+TEST_F(BoardTest, checkBoard)
+{
+    EXPECT_EQ(true, false);
+}
