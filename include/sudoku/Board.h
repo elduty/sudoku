3090f03c33a3bdd05d95eb083d37406a4fcb6531
Marcin <marcin.mrmartian@gmail.com>
2022-10-25 00:32:46 +0100

    interface to return rows, columns and quadrants

--- include/sudoku/Board.h
+++ include/sudoku/Board.h
@@ -19,10 +19,18 @@ class Board
 private:
     std::unique_ptr<std::vector<unsigned int>> _boardData;
 public:
-    static const unsigned int BOARD_WIDTH = 9;
-    static const unsigned int BOARD_HIGHT = 9;
+    static const unsigned int BOARD_DIMENSION = 9;
     Board();
     ~Board();
+    
+    const std::vector<unsigned int> getRow(unsigned int) const;
+    const std::vector<unsigned int> getColumn(unsigned int) const;
+    const std::vector<unsigned int> getQuadrant(unsigned int) const;
+    
+    unsigned int getRowForIndex(unsigned int) const;
+    unsigned int getColumnForIndex(unsigned int) const;
+    unsigned int getQuadrantForIndex(unsigned int) const;
+    
     friend std::ostream& operator<<(std::ostream&, const Board&);
     inline const std::vector<unsigned int>& getBoardData() const {return *_boardData;}
 };
--- src/sudoku/Board.cpp
+++ src/sudoku/Board.cpp
@@ -12,10 +12,13 @@ using namespace sudoku;
 
 Board::Board():_boardData(std::make_unique<std::vector<unsigned int>>())
 {
-    _boardData->reserve(BOARD_WIDTH * BOARD_HIGHT);
-    for(unsigned int y = 0; y < BOARD_HIGHT; ++y)
-        for(unsigned int x = 0; x < BOARD_WIDTH; ++x)
+    _boardData->reserve(BOARD_DIMENSION * BOARD_DIMENSION);
+    
+    for(unsigned int y = 0; y < BOARD_DIMENSION; ++y)
+        for(unsigned int x = 0; x < BOARD_DIMENSION; ++x)
+        {
             _boardData->push_back(x + 1);
+        }
 }
 
 Board::~Board()
@@ -23,15 +26,54 @@ Board::~Board()
     
 }
 
+const std::vector<unsigned int> Board::getRow(unsigned int index) const
+{
+    std::vector<unsigned int> row;
+    
+    for(unsigned int x = 0; x < BOARD_DIMENSION; ++x)
+        row.push_back(_boardData->at(index * BOARD_DIMENSION + x));
+    
+    return row;
+}
+const std::vector<unsigned int> Board::getColumn(unsigned int index) const
+{
+    std::vector<unsigned int> column;
+    
+    for(unsigned int y = 0; y < BOARD_DIMENSION; ++y)
+        column.push_back(_boardData->at(y * BOARD_DIMENSION + index));
+    
+    return column;
+}
+const std::vector<unsigned int> Board::getQuadrant(unsigned int) const
+{
+    std::vector<unsigned int> quadrat = {0,0,0,0,0,0,0,0,0};
+    return quadrat;
+}
+
+unsigned int Board::getRowForIndex(unsigned int index) const
+{
+    return index / BOARD_DIMENSION;
+}
+
+unsigned int Board::getColumnForIndex(unsigned int index) const
+{
+    return index % BOARD_DIMENSION;
+}
+
+unsigned int Board::getQuadrantForIndex(unsigned int index) const
+{
+    return 0;
+}
+
 std::ostream& sudoku::operator<<(std::ostream& os, const Board& board)
 {
     os << "-------------------" << std::endl;
-    for(unsigned int y = 0; y < Board::BOARD_HIGHT; ++y)
+    for(unsigned int y = 0; y < Board::BOARD_DIMENSION; ++y)
     {
         os << '|';
-        for(unsigned int x = 0; x < Board::BOARD_WIDTH; ++x)
+        for(unsigned int x = 0; x < Board::BOARD_DIMENSION; ++x)
         {
-            os << board._boardData->at(y * Board::BOARD_HIGHT + x);
+            os << board._boardData->at(y * Board::BOARD_DIMENSION + x);
             if(x%3 == 2)
                 os << '|';
             else
